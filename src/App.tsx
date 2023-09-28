97bb91466214eeb802beed0bc11341feae378309
mihail323i21 <mihail323i21@gmail.com>
2023-09-28 15:49:06 +0500

    test server + upload players from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        const jsonUrl = 'http://localhost:3000/users';
+        axios.get(jsonUrl).then((response) => {
+            const allUsers = response.data
+            setUsers(allUsers)
+        })
+    }, [])
+
+
     return (
         <div>
-
+            {users !== undefined
+                ? users.map((user) => (
+                    <div><img
+                        src={user.avatarImg}
+                        height="50px"
+                        alt={user.nickname}/> {user.id}.{user.nickname}</div>)
+                )
+                : "Пользователей нет"}
         </div>
     )
 }
