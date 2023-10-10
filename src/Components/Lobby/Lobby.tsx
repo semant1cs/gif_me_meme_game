1323505f8c0c54dfb147bd75829d3294ae6f5f10
Ovinkin Kirill <103418173+Randosky@users.noreply.github.com>
2023-10-10 13:24:10 +0500

    Создал верстку страниц WelcomePage и Lobby

--- src/Components/Lobby/Lobby.tsx
+++ src/Components/Lobby/Lobby.tsx
@@ -1,27 +1,61 @@
 import React, {useEffect} from "react";
-import game from "../../Store/GameStore";
+// import game from "../../Store/GameStore";
 import {observer} from 'mobx-react-lite';
-import {IUserType} from "../../Types/UserType";
+// import {IUserType} from "../../Types/UserType";
 import "../../Styles/LobbyStyle/Lobby.scss";
+import MyButton from "../../UI/MyButton";
+import UserIcon from "../../Imgs/SVG/UserIcon";
 
 const Lobby: React.FC = observer(() => {
 
     useEffect(() => {
-        game.getUsers()
     }, [])
 
     return (
         <main className="lobby">
-            <div className="lobby__container container">
-                {
-                    game.users
-                        ? game.users.map((user: IUserType, ind: number) =>
-                            <div key={ind}>
-                                <img src={user.avatarImg} height="50px" alt={user.nickname}/>
-                                {user.id}.{user.nickname}
-                            </div>)
-                        : "Пользователей нет"
-                }
+            <div className="lobby__container">
+                <header className="lobby__header">
+                    <ul className="header__list">
+                        <li>
+                            Друзья
+                        </li>
+                    </ul>
+                    <div className="header__user">
+                        <span>
+                            username
+                        </span>
+                        <UserIcon/>
+                    </div>
+                </header>
+                <div className="lobby__main">
+                    <section className="lobby__chats">
+                        <div className="chats__head">
+                            <h2>
+                                Чаты
+                            </h2>
+                            <ul className="chats__list">
+                                <li className="chats__chat active">общий</li>
+                                <li className="chats__chat">лобби</li>
+                            </ul>
+                        </div>
+                        <div className="chats__main">
+
+                        </div>
+                    </section>
+                    <section className="lobby__lobbies">
+                        <div className="lobbies__header">
+                            <h2>
+                                Ожидают игры
+                            </h2>
+                            <MyButton btnText="Создать игру"
+                                      btnStyle="lobbies__button"
+                                      handleOnClick={() => {}}/>
+                        </div>
+                        <div className="lobbies__main">
+
+                        </div>
+                    </section>
+                </div>
             </div>
         </main>
     );
---
+++ src/Imgs/SVG/UserIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserIcon: React.FC = () => {
+    return (
+        <svg width="50" height="51" viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M36.7188 46.4844C32.4219 49.6094 24.8016 49.6094 24.8016 49.6094C24.8016 49.6094 17.5781 49.6094 12.8906 46.4844C12.8906 37.4326 18.0076 27.3438 24.8016 27.3438C31.5956 27.3438 36.7188 37.4326 36.7188 46.4844Z"
+                stroke="#102031"/>
+            <circle cx="25" cy="17.9688" r="8.875" stroke="#102031"/>
+            <circle cx="25" cy="25" r="24.5" stroke="#102031"/>
+        </svg>
+    );
+};
+
+export default UserIcon;
