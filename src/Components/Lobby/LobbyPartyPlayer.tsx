b754a0a63ecd44ff570c2774cb3b763ce2e4f1f5
mihail323i21 <mihail323i21@gmail.com>
2023-10-22 21:08:03 +0500

    кнопка удаления появляется только у себя

--- src/Components/Lobby/LobbyPartyPlayer.tsx
+++ src/Components/Lobby/LobbyPartyPlayer.tsx
@@ -5,19 +5,19 @@ import {IUserType} from "../../Types/UserType";
 import AddPlayerLogo from "../../Imgs/SVG/AddPlayerLogo.tsx";
 import {ILobbyType} from "../../Types/LobbyType.ts";
 import lobbyStore from "../../Store/LobbyStore.ts";
+import {getAuth} from "firebase/auth";
 
-type LobbyUserProps =
-    {
-        player: IUserType,
-        lobbyInfo: ILobbyType
-    }
+type LobbyUserProps = {
+    player: IUserType,
+    lobbyInfo: ILobbyType
+}
 
 const LobbyPartyPlayer: React.FC<LobbyUserProps> = observer(({player, lobbyInfo}: LobbyUserProps) => {
+    const auth = getAuth()
+
     return (
         <div className="party__player">
-            <button className="leave_lobby__btn" onClick={() => lobbyStore.removePlayerFromParty(lobbyInfo, player)}>
-                <AddPlayerLogo/>
-            </button>
+            {auth.currentUser?.uid === player.id ? <LeavePlayerButton lobbyInfo={lobbyInfo} player={player}/> : ""}
             {
                 player.photoURL
                     ? <img className="player_avatar" src={player.photoURL} alt="playerAvatar"/>
@@ -29,5 +29,15 @@ const LobbyPartyPlayer: React.FC<LobbyUserProps> = observer(({player, lobbyInfo}
         </div>
     );
 })
+
+const LeavePlayerButton = ({player, lobbyInfo}: LobbyUserProps) => {
+    return (
+        <div>
+            <div className="leave_lobby__btn" onClick={() => lobbyStore.removePlayerFromParty(lobbyInfo, player)}>
+                <AddPlayerLogo/>
+            </div>
+        </div>
+    );
+};
 
 export default LobbyPartyPlayer;
