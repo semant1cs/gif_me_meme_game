63796dd1e746ac4b9575ba3d890fe40cc4fd17af
Ovinkin Kirill <103418173+Randosky@users.noreply.github.com>
2023-10-22 22:45:54 +0500

    Проверил работоспособность выхода пользователя из лобби, доработал добавление и наведение курсора на игрока (стили)

--- src/Components/Lobby/LobbyPartyPlayer.tsx
+++ src/Components/Lobby/LobbyPartyPlayer.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {observer} from "mobx-react-lite";
 import UserIcon from "../../Imgs/SVG/UserIcon";
 import {IUserType} from "../../Types/UserType";
-import AddPlayerLogo from "../../Imgs/SVG/AddPlayerLogo.tsx";
 import {ILobbyType} from "../../Types/LobbyType.ts";
 import lobbyStore from "../../Store/LobbyStore.ts";
 import {getAuth} from "firebase/auth";
@@ -14,14 +13,25 @@ type LobbyUserProps = {
 
 const LobbyPartyPlayer: React.FC<LobbyUserProps> = observer(({player, lobbyInfo}: LobbyUserProps) => {
     const auth = getAuth()
+    const isUser = auth.currentUser?.uid === player.id
+
+    const handleOnRemove = (lobbyInfo: ILobbyType, player: IUserType) => {
+        if (isUser)
+            lobbyStore.removePlayerFromParty(lobbyInfo, player).then()
+    }
 
     return (
-        <div className="party__player">
-            {auth.currentUser?.uid === player.id ? <LeavePlayerButton lobbyInfo={lobbyInfo} player={player}/> : ""}
+        <div className="party__player"
+             onClick={() => handleOnRemove(lobbyInfo, player)}>
             {
                 player.photoURL
-                    ? <img className="player_avatar" src={player.photoURL} alt="playerAvatar"/>
-                    : <UserIcon/>
+                    ?
+                    <img className={`player_avatar ${isUser ? "party__playerLeave" : ""}`}
+                         src={player.photoURL} alt="playerAvatar"/>
+                    :
+                    <div className={`player_avatar ${isUser ? "party__playerLeave" : ""}`}>
+                        <UserIcon/>
+                    </div>
             }
             <p className="player__name">
                 {player.nickname}
@@ -29,15 +39,5 @@ const LobbyPartyPlayer: React.FC<LobbyUserProps> = observer(({player, lobbyInfo}
         </div>
     );
 })
-
-const LeavePlayerButton = ({player, lobbyInfo}: LobbyUserProps) => {
-    return (
-        <div>
-            <div className="leave_lobby__btn" onClick={() => lobbyStore.removePlayerFromParty(lobbyInfo, player)}>
-                <AddPlayerLogo/>
-            </div>
-        </div>
-    );
-};
 
 export default LobbyPartyPlayer;
