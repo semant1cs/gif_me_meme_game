4127c63b23f694fc18a1aec7af501bf3dc9eaf94
Ovinkin Kirill <103418173+Randosky@users.noreply.github.com>
2023-11-24 17:26:36 +0500

    Продолжаю дополнять функционал. Делаю возможность ставить реакции

--- src/Components/Game/GameStages/Reaction/GameSendReactionStage.tsx
+++ src/Components/Game/GameStages/Reaction/GameSendReactionStage.tsx
@@ -1,29 +1,52 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {observer} from "mobx-react-lite";
 import {Swiper, SwiperSlide} from "swiper/react";
 import answerStore from "../../../../Store/GameStores/AnswerStore";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import situationStore from "../../../../Store/GameStores/SituationStore";
+import {reactions} from "../../../../assets/assetsPicker";
 
 
 const GameSendReactionStage: React.FC = observer(() => {
+    const [currentSlide, setCurrentSlide] = useState<number>(0);
+
     useEffect(() => {
-    })
+        console.log(currentSlide)
+    }, [currentSlide])
+
+    const swiperSlides = Array.from(answerStore.allLobbySituationAnswers, answer =>
+        <SwiperSlide key={answer.answerId}>
+            <div className="reactions__gif">
+                <img src={answer.answerGif?.gif || answer.answerGif?.mediumGif}
+                     alt="answerGif"/>
+            </div>
+        </SwiperSlide>
+    )
+
+    const reactionsArray = Array.from(reactions, (reaction, index) =>
+        <img key={index} src={reaction.imgSrc.default}
+             alt="reactionImage"
+             className="reactions__reaction"/>
+    )
 
     return (
         <section className="game__reactions">
-            <Swiper
-                spaceBetween={20}
-                slidesPerView={1}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                {
-                    answerStore.allLobbySituationAnswers.map(answer =>
-                        <SwiperSlide key={answer.answerId}>
-                            <img src={answer.answerGif?.mediumGif} alt="answerGif"/>
-                        </SwiperSlide>
-                    )
-                }
-            </Swiper>
+            <p className="reactions__title">
+                {situationStore.currentRoundSituation?.situationText || ""}
+            </p>
+                <Swiper spaceBetween={30}
+                        slidesPerView={1}
+                        onSlideChange={(swiper) => setCurrentSlide(swiper.activeIndex)}
+                        navigation={{nextEl: '.reactions__reaction'}}
+                        modules={[Navigation]}
+                        className="reactions__swiper">
+                    {swiperSlides ? swiperSlides : ""}
+                </Swiper>
+            <div className="reactions__reactions">
+                {reactionsArray ? reactionsArray : ""}
+            </div>
         </section>
     );
 });
